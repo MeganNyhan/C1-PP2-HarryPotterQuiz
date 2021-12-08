7ca1b994e75a9a7fbfa5ccdad2aedaf70fe9a145
MeganNyhan <nyhanmegan@gmail.com>
2021-12-08 21:44:04 +0000

    Set show info box comand

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,10 @@ const quiz_box = document.querySelector(".quiz_box");
 const result_box = document.querySelector(".result_box");
 const option_list = document.querySelector(".option_list");
 const time_line = document.querySelector("header .time_line");
-const timeText = document.querySelector(".timer .timer_left_txt");
+const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
+
+//When "start quiz" button is clicked:
+start_btn.onclick = () =>{
+    info_box.classList.add("activeInfo");
+}
