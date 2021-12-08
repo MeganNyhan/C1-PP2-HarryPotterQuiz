192bf8927a6765645c65a46d79321ce9eecfcb48
MeganNyhan <nyhanmegan@gmail.com>
2021-12-08 21:36:02 +0000

    Add questions to questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,114 @@
+// JavaScript array for questions and answers:
+
+let questions = [
+    {
+        numb:1,
+        question: "What does Harry accidentally do when he goes to the zoo?",
+        answer: "Make the glass in the snake enclosure disappear?",
+        options:[
+            "Make the glass in the snake enclosure disappear?",
+            "Teaches the monkeys to sing?",
+            "Turns a flamingo blue?",
+            "Turns a meerkat into a football?"
+        ]
+    },
+    {
+        numb:2,
+        question: "What's Harry's mum Lily's surname before she marries Harry's dad?",
+        answer: "Evans?",
+        options:[
+            "Peters?",
+            "Smith?",
+            "Evans?",
+            "Collins?"
+        ]
+    },
+    {
+        numb:3,
+        question: "What's the name of Dudley Dursely's dad?",
+        answer: "Vernon?",
+        options:[
+            "Victor?",
+            "Ivan?",
+            "Vernon?",
+            "Vesuvius?"
+        ]
+    },
+    {
+        numb:4,
+        question: "How many Harry Potter books are there in total?",
+        answer: "7?",
+        options:[
+            "2?",
+            "7?",
+            "12?",
+            "1?"
+        ]
+    },
+    {
+        numb:5,
+        question: "In which year was Harry born?",
+        answer: "1980?",
+        options:[
+            "1991?",
+            "1976?",
+            "2002?",
+            "1980?"
+        ]
+    },
+    {
+        numb:6,
+        question: "What shape scar does Harry have on his forehead?",
+        answer: "A lightning bolt?",
+        options:[
+            "A cat?",
+            "A moon?",
+            "A fidget spinner?",
+            "A lightning bolt?"
+        ]
+    },
+    {
+        numb:7,
+        question: "Who does Harry live with before going to Hogwarts?",
+        answer:  "The Dursleys?",
+        options:[
+            "The Simpsons?",
+            "The Menaces?",
+            "The Dursleys?",
+            "The Sheerans?"
+        ]
+    },
+    {
+        numb:8,
+        question: "What is a 'muggle?",
+        answer:  "Someone whose parents aren't magical and can't perform magic?",
+        options:[
+            "A type of puppet?",
+            "A type of cup?",
+            "Someone whose parents aren't magical and can't perform magic?",
+            "A type of outrageous haircut?"
+        ]
+    },
+    {
+        numb:9,
+        question: "What is the name of Hermione Granger's cat?",
+        answer:  "Crookshanks?",
+        options:[
+            "Bart?",
+            "Crookshanks?",
+            "Peppa?",
+            "Sarah?"
+        ]
+    },
+    {
+        numb:10,
+        question: "What’s the name of Hogwarts School’s gamekeeper?",
+        answer:  "Rubeus Hagrid?",
+        options:[
+            "Ralph?",
+            "Ted?",
+            "Homer?",
+            "Rubeus Hagrid?"
+        ]
+    },
+];
