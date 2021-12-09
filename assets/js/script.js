e5aad4e584ad38adb22b32e45fd8e2ac321288f9
MeganNyhan <nyhanmegan@gmail.com>
2021-12-09 17:21:25 +0000

    Add questions to javascript from new javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
 
 //When "Start Quiz" button clicked
 start_btn.onclick = ()=>{
@@ -16,6 +17,15 @@ exit_btn.onclick = ()=>{
 
 // When "Continue Quiz" button clicked
 continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); 
     quiz_box.classList.add("activeQuiz"); 
 }
+
+//Questions and Answers from Array
+let que_count = 0;
+
+function showQuestions(){
+    const que_text = document.querySelector("que_text")
+}
+
 
