1ecdfd741924fbf4592164b87e652fd8826fa51f
MeganNyhan <nyhanmegan@gmail.com>
2021-12-21 12:27:59 +0000

    Add chnages to email js file

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,19 +1,18 @@
 /*
 * This function sends an email to the user from the site, through emailJS.
 * calls the function sendMail()
-* @param {string: Params} - found in the html file.
 */
 
-function sendMail(params){
+function sendMail(){
   var tempParams = {
     from_name: document.getElementById("fromName").value,
     to_name: document.getElementById("toName").value,
     message: document.getElementById("msg").value,
   };
-  emailjs.send('service_va76r2o', 'template_wh1kqsp', tempParams)
-  .then((res) => {
+  emailjs.send('gmail', 'template_ry67bk5', tempParams)
+  .then(function(res) {
     window.alert("Your email has been sent!")
-  }
-  )}
+  })
+}
 
 
