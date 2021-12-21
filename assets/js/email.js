aaaee30d3a26450eda2b386c592529f6df6c5414
MeganNyhan <nyhanmegan@gmail.com>
2021-12-21 15:24:31 +0000

    Add changes to email js

--- assets/js/email.js
+++ assets/js/email.js
@@ -3,3 +3,15 @@
 * calls the function sendMail()
 */
 
+function sendMail(){
+  let tempParams ={
+    from_name: document.getElementById("fromName").value,
+    to_name: document.getElementById("toName").value,
+    message: document.getElementById("msg").value,
+ };
+
+ emailjs.send('service_f60q3jq', 'template_oebyimc', tempParams)
+  .then(function(res){
+    console.log("success", res.status);
+  })
+}
